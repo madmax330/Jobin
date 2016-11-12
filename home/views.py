eb3fb9c363ec7966fe369f7eb1d070bef839b817
Merry M <mcmutima@gmail.com>
2016-11-12 01:42:26 -0500

    add email verification
    Ajout de l'envoi de mail et de la vérification du coté serveur pour les
    étudiants et la company

--- home/views.py
+++ home/views.py
@@ -1,13 +1,20 @@
+from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group , User
 from .forms import NewUserForm
-from .models import JobinSchool, Notification, JobinRequestedEmail, Message, JobinBlockedEmail
+from .models import JobinSchool, Notification, JobinRequestedEmail, Message, JobinBlockedEmail,JobinInvalidUser
 from student.models import Student
 from company.models import Company
 from django.views.generic import View
+from datetime import datetime
+from home.token import generate_confirmation_token, confirm_token
+from home.email import send_email
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
-
+request = HttpRequest()
 class IndexView(View):
     template_name = 'home/home.html'
 
@@ -27,6 +34,9 @@ class IndexView(View):
                     return redirect('company:index')
                 elif user.groups.filter(name='student_user').exists():
                     return redirect('student:index')
+                elif user.groups.filter(name='unvalid_user').exists():
+                    infos = 'Your account is not verified yet, please confirm your account with the link sent to you by mail'
+                    return redirect('home:invalid_user', Infos =infos )
                 else:
                     return redirect('home:index')
         return render(request, self.template_name, {'error': 'Username or password is incorrect.'})
@@ -45,6 +55,7 @@ class RegisterView(View):
         }
         return render(request, self.template_name, context)
 
+
     # process form data
     def post(self, request, utype):
         form = self.form_class(request.POST, utype=utype)
@@ -55,18 +66,41 @@ class RegisterView(View):
             password = form.cleaned_data['password']
             user.set_password(password)
             user.save()
+
+
+            #Temp user create
+            temp_user =  JobinInvalidUser()
+
             if utype == 'company':
-                g = Group.objects.get(name='company_user')
+                temp_user.user = user
+                temp_user.category = 'company'
+                temp_user.date = datetime.now()
+                temp_user.save()
+                token = generate_confirmation_token(user.email)
+                url = request.build_absolute_uri()
+                confirm_url = url + 'confirm_email/' + token
+                html = render_to_string('home/active.html', {'confirm_url': confirm_url})
+                text_content = strip_tags(html)
+                subject = "Please confirm your email"
+                send_email(user.email, subject, html, text_content)
+                g = Group.objects.get(name='unvalid_user')
                 g.user_set.add(user)
+
             elif utype == 'student':
-                g = Group.objects.get(name='student_user')
+                temp_user.user = user
+                temp_user.category ='student'
+                temp_user.date = datetime.now()
+                temp_user.save()
+                token = generate_confirmation_token(user.email)
+                url = request.build_absolute_uri()
+                confirm_url = url + 'confirm_email/' + token
+                html = render_to_string('home/active.html', {'confirm_url': confirm_url})
+                text_content = strip_tags(html)
+                subject = "Please confirm your email"
+                send_email(user.email, subject, html,text_content )
+                g = Group.objects.get(name='unvalid_user')
                 g.user_set.add(user)
-                ext = user.email.split('@', 1)[1]
-                ems = JobinSchool.objects.filter(email=ext.lower())
-                if ems.count() == 0:
-                    x = JobinRequestedEmail()
-                    x.extension = ext.lower()
-                    x.save()
+
             else:
                 return redirect('home:index')
             return redirect('home:verify')
@@ -179,6 +213,11 @@ class VerifyView(View):
     def get(self, request):
         return render(request, self.template_name)
 
+class UnvalidUser(View):
+    template_name = 'home/invalid_user.html'
+
+    def get(self, request,Infos):
+        return render(request, self.template_name, {'Infos': Infos})
 
 class LogoutView(View):
 
@@ -218,3 +257,42 @@ class CloseAllNotifications(View):
             return redirect('student:index')
         logout(request)
         return redirect('home:index')
+
+
+def confirm_email(request,token):
+    try:
+        email = confirm_token(token)
+    except:
+        infos = 'The confirmation link is invalid or has expired.'
+        return redirect('home:invalid_user', Infos=infos)
+    user_auth = User.objects.get(username=email)
+    if not user_auth.groups.filter(name='unvalid_user').exists():
+        infos = 'Account already confirmed. Please login.'
+        return redirect('home:invalid_user', Infos=infos)
+    else:
+
+        try:
+          user = JobinInvalidUser.objects.get(user=email)
+        except:
+            infos = 'User ncannot be found!'
+            return redirect('home:invalid_user', Infos=infos)
+
+        if user.category == 'company':
+         g = Group.objects.get(name='company_user')
+         g.user_set.add(user_auth)
+        elif user.category == 'student':
+            g = Group.objects.get(name='student_user')
+            g.user_set.add(user_auth)
+            ext = user_auth.email.split('@', 1)[1]
+            ems = JobinSchool.objects.filter(email=ext.lower())
+            if ems.count() == 0:
+                x = JobinRequestedEmail()
+                x.extension = ext.lower()
+                x.save()
+        g = Group.objects.get(name='unvalid_user')
+        g.user_set.remove(user_auth)
+        infos = 'You have confirmed your account. Thanks!'
+
+    return redirect('home:invalid_user', Infos=infos)
+
+
